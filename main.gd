0bfb0131eea7e0b65bfbbe99dcb799391b695361
Karachevtsev Yu Yu <grammidin4eg@gmail.com>
2024-01-14 13:46:53 +0300

    _check_win

--- main.gd
+++ main.gd
@@ -5,7 +5,7 @@ var currentPlayer: EPlayerType
 @export var blocks: Array[Sprite2D]
 
 func _ready():
-	currentPlayer = EPlayerType.CROSS
+	_new_game()
 
 func _reverse_player_type():
 	currentPlayer = EPlayerType.CROSS if currentPlayer == EPlayerType.ZERO else EPlayerType.ZERO
@@ -15,6 +15,44 @@ func _on_block_click(curBlock):
 	if curBlock.is_empty():
 		curBlock.set_value(currentPlayer)
 		_reverse_player_type()
+		_check_win()
 
 func _check_win() -> bool:
+	const checkArray = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4 ,6]
+	]
+	for i in len(checkArray):
+		var result = _check_line(checkArray[i])
+		if result != EPlayerType.EMPTY:
+			print("!!! win: ", result)
+			_game_over(result)
 	return false
+
+func _check_line(row: Array) -> EPlayerType:
+	var row_sum = 0
+	for i in len(row):
+		row_sum += _get_block_value(row[i])
+	if row_sum == 3:
+		return EPlayerType.CROSS
+	elif row_sum == -3:
+		return EPlayerType.ZERO
+	return EPlayerType.EMPTY
+
+func _get_block_value(index: int) -> EPlayerType:
+	return blocks[index].get_value()
+
+func _game_over(result: EPlayerType):
+	get_tree().paused = true
+	pass
+
+func _new_game():
+	currentPlayer = EPlayerType.CROSS
+	get_tree().paused = false
+	pass
